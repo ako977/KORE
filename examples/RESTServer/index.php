e999d8d88bf82884f3f6119aa9e145801a965384
E-Twenty Development <a.oseigyau@etwenty.co>
2012-09-03 14:37:49 -0400

    added slim PHP server code for REST server example

---
+++ examples/RESTServer/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require "movie-library/config.php";
+
+var_dump(DOCUMENT_ROOT);
---
+++ examples/RESTServer/movie-library/config.php
@@ -0,0 +1,4 @@
+<?php
+
+//Config vars for project
+define( 'DOCUMENT_ROOT', "D:\web_projects\KORE\examples\RESTServer" );
