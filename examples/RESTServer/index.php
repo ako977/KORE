bf9c0089067bd0ec8a7c12ec735da3bc302f2a39
E-Twenty Development <a.oseigyau@etwenty.co>
2012-09-03 23:47:57 -0400

    configuring PHP RESTful server

--- examples/RESTServer/index.php
+++ examples/RESTServer/index.php
@@ -2,4 +2,4 @@
 
 require "movie-library/config.php";
 
-var_dump(DOCUMENT_ROOT);
+var_dump( Config::$DB->query("PRAGMA table_info(movie_library)")->fetchAll(PDO::FETCH_ASSOC) );
--- examples/RESTServer/movie-library/config.php
+++ examples/RESTServer/movie-library/config.php
@@ -1,4 +1,41 @@
 <?php
 
 //Config vars for project
-define( 'DOCUMENT_ROOT', "D:\web_projects\KORE\examples\RESTServer" );
+define( 'DOCUMENT_ROOT', dirname(__DIR__) );
+
+require DOCUMENT_ROOT."/../vendors/codeguy-Slim-18d6d03/Slim/Slim.php";
+
+class Config 
+{
+	
+	public static $RestApp;
+	public static $DB;
+
+	public static function initSlim()
+	{
+		self::$RestApp = new Slim();
+	}
+
+	public function __autoload( $class_name )
+	{
+		include DOCUMENT_ROOT.'/movie-library/classes/'.$class_name.'.php';
+	}
+
+	public static function initDatabase()
+	{			
+		self::$DB = new PDO( 'sqlite:'.DOCUMENT_ROOT.'/movie-library/movie-library-db.sq3', null, null );
+
+		self::$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		self::$DB->exec("CREATE TABLE IF NOT EXISTS movie_library (
+                    id INTEGER PRIMARY KEY, 
+                    title TEXT, 
+                    format TEXT, 
+                    length INTEGER,
+                    rating INTEGER)");
+	}
+}
+
+Config::initSlim();
+
+Config::initDatabase();
